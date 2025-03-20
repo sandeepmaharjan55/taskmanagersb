21318c77acd6de684b0efe95c1732f830705997b
Sandeep Maharjan <sandeepmaharjan55@gmail.com>
2025-03-20 10:22:46 -0500

    TaskModel added

---
+++ src/main/java/com/taskmanager/model/Task.java
@@ -0,0 +1,31 @@
+package com.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDateTime dueDate;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private String priority;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
